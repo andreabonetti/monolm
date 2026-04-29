80128081f383163598d1c731886df72709ae4491
Andrea Bonetti <andreabonetti@gmail.com>
2026-04-29 23:02:12 +0200

    cleaner chat code

--- chat.py
+++ chat.py
@@ -3,25 +3,15 @@
 from monolm import chat, load_model, url_context
 
 if __name__ == '__main__':
-    config = {
-        'model': 'models/gemma-4-E4B-it-Q4_K_M.gguf',
-        'n_ctx': 131072,
-        'n_threads': 8,
-        'n_gpu_layers': 43,
-    }
-
     llm = load_model(
-        model_path=config['model'],
-        n_ctx=config['n_ctx'],
-        n_threads=config['n_threads'],
-        n_gpu_layers=config['n_gpu_layers'],
+        model_path='models/gemma-4-E4B-it-Q4_K_M.gguf',
+        n_ctx=131072,
+        n_threads=8,
+        n_gpu_layers=43,
     )
 
     tools = [url_context]
 
-    print('monolm chat')
-    print(f'→ llm: {config["model"]}')
-    print(f'→ tools: {", ".join([tool.__name__ for tool in tools])}')
-    print("type 'exit' to quit.\n")
+    print("monolm chat - type 'exit' to quit.\n")
 
     chat(llm, tools=[url_context])
