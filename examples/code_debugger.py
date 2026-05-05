83cea57b0eda2071b63841c1e1c936f16dbfc67f
Andrea Bonetti <andreabonetti@gmail.com>
2026-05-05 08:16:07 +0200

    update code debugger to remove url_context and read tools

--- examples/code_debugger.py
+++ examples/code_debugger.py
@@ -1,13 +1,13 @@
 """code debugger"""
 
-from monolm import chat, load_config, load_model, read, url_context, write_stream, write_user
+from monolm import chat, load_config, load_model, write_stream, write_user
 
 if __name__ == '__main__':
     config = load_config()
 
     llm = load_model(config)
 
-    tools_user = [url_context, read, write_user]
+    tools_user = [write_user]
     tools_stream = [write_stream]
 
     prompt = 'debug /write data/bugged_hello_world.py'
